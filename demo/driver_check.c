d8204bc798e0874c08be57f40c6b0a637f9a32c6
irrenium <reyad.ouahi@ens.uvsq.fr>
2025-02-20 10:57:06 +0100

    fix clock driver_check

--- demo/driver_check.c
+++ demo/driver_check.c
@@ -2,25 +2,32 @@
 #include <stdlib.h> // atoi
 #include <stdint.h>
 
-extern uint64_t rdtsc ();
+//extern uint64_t rdtsc ();
 
 // TODO: adjust for each kernel
 extern void kernel (unsigned n, float a[n][n], float b[n][n], float c[n][n]);
 
 // TODO: adjust for each kernel
-static void init_array (int n, float a[n][n]) {
+static void init_array_2 (int n, float x[n][n]) {
    int i, j;
 
    for (i=0; i<n; i++)
       for (j=0; j<n; j++)
-         a[i][j] = (float) rand() / RAND_MAX;
+         x[i][j] = (float) rand() / RAND_MAX;
+}
+
+static void init_array_1 (int n, float a[n]) {
+   int i;
+
+   for (i=0; i<n; i++)
+         a[i] = (float) rand() / RAND_MAX;
 }
 
 // TODO: adjust for each kernel
-static void print_array (int n, float a[n][n], const char *output_file_name) {
+static void print_array_2 (int n, float a[n][n], const char *output_file_name) {
    int i, j;
 
-   FILE *fp = fopen (output_file_name, "w");
+   FILE *fp = fopen (output_file_name, "w+");
    if (fp == NULL) {
       fprintf (stderr, "Cannot write to %s\n", output_file_name);
       return;
@@ -29,6 +36,21 @@ static void print_array (int n, float a[n][n], const char *output_file_name) {
    for (i=0; i<n; i++)
       for (j=0; j<n; j++)
          fprintf (fp, "%f\n", a[i][j]);
+
+   fclose (fp);
+}
+
+static void print_array_1 (int n, float a[n], const char *output_file_name) {
+   int i;
+
+   FILE *fp = fopen (output_file_name, "w+");
+   if (fp == NULL) {
+      fprintf (stderr, "Cannot write to %s\n", output_file_name);
+      return;
+   }
+
+   for (i=0; i<n; i++)
+         fprintf (fp, "%f\n", a[i]);
 
    fclose (fp);
 }
@@ -45,18 +67,19 @@ int main (int argc, char *argv[]) {
    const char *output_file_name = argv[2];
 
    /* allocate arrays. TODO: adjust for each kernel */
-   float (*a)[size] = malloc (size * size * sizeof a[0][0]);
-   float (*b)[size] = malloc (size * size * sizeof b[0][0]);
+   float *a = malloc (size * sizeof a[0]);
+   float *b = malloc (size * sizeof b[0]);
    float (*c)[size] = malloc (size * size * sizeof c[0][0]);
 
    /* init arrays */
    srand(0);
-   init_array (size, a);
-   init_array (size, b);
+   init_array_1 (size, a);
+   init_array_1 (size, b);
+   init_array_2 (size, c);
 
    /* print output */
    kernel (size, a, b, c);
-   print_array (size, c, output_file_name);
+   print_array_2 (size, c, output_file_name);
 
    /* free arrays. TODO: adjust for each kernel */
    free (a);
