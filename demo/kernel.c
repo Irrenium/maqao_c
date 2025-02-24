d2e426eff237b6576ff5aea4fb61668c6e245162
irrenium <reyad.ouahi@ens.uvsq.fr>
2025-02-24 15:12:16 +0100

    open MP versions

--- demo/kernel.c
+++ demo/kernel.c
@@ -1,18 +1,26 @@
 #ifdef OPT1
 
 /* Removing of store to load dependency (array ref replaced by scalar) */
-void kernel (unsigned n, float a[n], float b[n], float c[n][n]) {
-   for (unsigned i = 0; i < n; i++) {
-      float temp  = a[i];
-      float inv_b = 1.0f / b[i];  // Only one division per row
-      float sum   = 0.0f;
+#include <omp.h>
+// n x n, row-major float matrix c
+// vectors a, b each of length n
+// We assume c is not constant across calls; otherwise, consider precomputing sums.
+void kernel(unsigned n, float a[n], float b[n], float c[n][n]) {
+#pragma omp parallel for  // parallelize over i
+    for (unsigned i = 0; i < n; i++) {
+        float temp  = a[i];
+        float inv_b = 1.0f / b[i];  // single division
+        float sum   = 0.0f;
 
-      for (unsigned j = 0; j < n; j++) {
+        // Potentially vectorizable
+#pragma omp simd reduction(+:sum)
+        for (unsigned j = 0; j < n; j++) {
             sum += c[i][j];
-      }
-      temp += sum * inv_b;
-      a[i] = temp;
-   }
+        }
+
+        temp += sum * inv_b;
+        a[i] = temp;
+    }
 }
 
 #elif defined OPT2
